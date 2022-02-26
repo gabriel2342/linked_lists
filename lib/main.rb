495a404a8a849b692d7b2d258ad14ce5edd5ef56
gabrielpendas42 <gabriel.pendas@gmail.com>
2022-02-26 07:29:16 -0700

    Completed extra credit methods remove_at(index) and insert_at(value,index) and ran rubocop

--- lib/main.rb
+++ lib/main.rb
@@ -1,6 +1,4 @@
-
 class LinkedList
-
   attr_accessor :head
 
   def initialize
@@ -10,28 +8,26 @@ class LinkedList
   def append(value)
     if head.nil?
       self.head = Node.new(value)
-    else  
-      last_node = self.head
-      until last_node.next_node == nil
-        last_node = last_node.next_node
-      end
+    else
+      last_node = head
+      last_node = last_node.next_node until last_node.next_node.nil?
       last_node.next_node = Node.new(value)
     end
   end
 
   def prepend(value)
-    if self.head.nil?
+    if head.nil?
       self.head = Node.new(value)
     else
-      old_head = self.head
+      old_head = head
       self.head = Node.new(value, old_head)
     end
   end
-      
+
   def size
     counter = 1
-    last_node = self.head
-    until last_node.next_node == nil
+    last_node = head
+    until last_node.next_node.nil?
       last_node = last_node.next_node
       counter += 1
     end
@@ -39,21 +35,20 @@ class LinkedList
   end
 
   def heads
-    self.head.value
+    head.value
   end
 
   def tail
-    last_node = self.head
-    until last_node.next_node == nil
-      last_node = last_node.next_node
-    end
+    last_node = head
+    last_node = last_node.next_node until last_node.next_node.nil?
     last_node.value
   end
 
   def at(index)
-    return if self.head.nil?
+    return if head.nil?
+
     counter = 0
-    last_node =self.head
+    last_node = head
     until counter == index
       last_node = last_node.next_node
       counter += 1
@@ -63,17 +58,17 @@ class LinkedList
 
   def pop
     return if head.nil?
-    last_node = self.head
-    until last_node.next_node.next_node == nil
-      last_node = last_node.next_node
-    end
+
+    last_node = head
+    last_node = last_node.next_node until last_node.next_node.next_node.nil?
     last_node.next_node = nil
   end
 
   def contains?(val)
     return if head.nil?
-    last_node = self.head
-    until last_node.next_node == nil
+
+    last_node = head
+    until last_node.next_node.nil?
       last_node = last_node.next_node
       return true if last_node.value == val
     end
@@ -82,44 +77,56 @@ class LinkedList
 
   def find(val)
     return if head.nil?
-    last_node = self.head
+
+    last_node = head
     counter = 0
-    until last_node.next_node == nil
+    until last_node.next_node.nil?
       last_node = last_node.next_node
-      counter +=1
+      counter += 1
       return counter if last_node.value == val
-      
+
     end
     nil
   end
 
   def to_s
-    last_node = self.head
-    until last_node == nil
+    last_node = head
+    until last_node.nil?
       last_node.value = last_node.value.to_s
       last_node = last_node.next_node
     end
   end
 
   def insert_at(value, index)
-    last_node = self.head
+    last_node = head
     previous_node = last_node
     counter = 0
     until counter == index
       previous_node = last_node
       last_node = last_node.next_node
-      counter +=1
+      counter += 1
     end
     our_node = Node.new(value)
     previous_node.next_node = our_node
     our_node.next_node = last_node
-    
   end
 
-  private 
+  def remove_at(index)
+    last_node = head
+    previous_node = last_node
+    counter = 0
+    until counter == index
+      previous_node = last_node
+      last_node = last_node.next_node
+      counter += 1
+    end
+    second_node = last_node.next_node
+    previous_node.next_node = second_node
+  end
 
   class Node
     attr_accessor :value, :next_node
+
     def initialize(value = nil, next_node = nil)
       self.value = value
       self.next_node = next_node
@@ -133,8 +140,9 @@ newlist.append(20)
 newlist.append(30)
 newlist.append(40)
 newlist.append(100)
-newlist.insert_at(23,2)
-#newlist.to_s
+newlist.insert_at(23, 2)
+newlist.remove_at(3)
+# newlist.to_s
 p newlist
 # p newlist.contains?(39)
 # p newlist.find(30)
